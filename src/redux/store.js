80e834de7e3954dc7c24dd0f658fdb83a1f12232
Pavel Belyaev <zommerberg@abv.bg>
2020-05-26 23:15:06 +0300

    remove dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import {applyMiddleware, createStore} from 'redux';
 
-import {composeWithDevTools} from 'redux-devtools-extension';
 import {offline} from '@redux-offline/redux-offline';
 import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
 import {rootReducer} from './reducer';
@@ -17,21 +16,20 @@ const setHydratedState = () => store.dispatch(setHydrated());
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(
-    offline({
-      ...offlineConfig,
-      persistOptions: {
-        blacklist: ['hydration'],
-      },
-      // this fires after store hydration is done
-      persistCallback: () => {
-        setLanguage();
-        setHydratedState();
-      },
-      retry: () => 300000, // retry  every 5 minutes
-    }),
-    applyMiddleware(thunk, submitDraftWithImages),
-  ),
+
+  offline({
+    ...offlineConfig,
+    persistOptions: {
+      blacklist: ['hydration'],
+    },
+    // this fires after store hydration is done
+    persistCallback: () => {
+      setLanguage();
+      setHydratedState();
+    },
+    retry: () => 300000, // retry  every 5 minutes
+  }),
+  applyMiddleware(thunk, submitDraftWithImages),
 );
 
 export default store;
